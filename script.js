4e84a17c3c147358ca8ee8d90ac793f7b604decc
StefanMartianov <156689031+StefanMartianov@users.noreply.github.com>
2024-01-31 18:07:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+/*  Programm 1
+const num = +prompt("Type number")
+sum = num * 0.1
+console.log(sum)
+*/
+
+/*  Programm 2
+const num1 = +prompt("Type number 1");
+const num2 = +prompt("Type number 2");
+if (num1 > num2) {
+  console.log(num1);
+} else if (num1 < num2) {
+  console.log(num2);
+}
+*/
+
+/*  Programm 3
+const number1 = +prompt("Type number");
+if (number1 > 100) {
+  console.log("The number is more than 100");
+} else if (number1 === 100) {
+  console.log("The number is equal to 100");
+} else if (number1 < 100) {
+  console.log("The number is less than 100");
+}
+*/
+
+/*  Programm 4
+const username = prompt("Type your name")
+const actualYear = 2024
+const birthYear = +prompt("Type your birth year")
+if (actualYear - birthYear < 18) {
+    console.log("Hi," + username)
+} else if (actualYear - birthYear > 18) {
+    console.log("Hello," + username)
+}
+*/
